725f8d7e411796edfa7141e848daea59e297f9f3
mikebruzenak <mike.bruzenak@gmail.com>
2014-07-05 11:34:42 -0500

    validations

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,4 @@
 class Order < ActiveRecord::Base
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create }
+  validates :phone_number, format: { with: /\d{3}-\d{3}-\d{4}/, message: "bad format" }
 end
